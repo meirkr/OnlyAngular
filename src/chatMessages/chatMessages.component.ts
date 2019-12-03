db5d9afeac1c14253f71af013ed5f2803351d626
Meir Kriheli <meir.kriheli@hp.com>
2019-12-03 10:54:57 +0200

    test: use labels to send move left/right to tobot

--- src/chatMessages/chatMessages.component.ts
+++ src/chatMessages/chatMessages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { HubConnection, HubConnectionBuilder, JsonHubProtocol } from '@aspnet/signalr';
 import { ChatLoginService } from '../login/chatLogin.service';
+import { stringify } from 'querystring';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'chatMessages',
@@ -42,7 +44,7 @@ export class ChatMessagesComponent implements OnInit {
     private reconnect() : void {
 
       this._hubConnection = new HubConnectionBuilder()
-        .withUrl('/chat')
+        .withUrl('/robot')
         .build();
 
       this._hubConnection
@@ -63,6 +65,16 @@ export class ChatMessagesComponent implements OnInit {
         this.messages.unshift(text);
       });
 
+      this._hubConnection.on('SendObjAsync', (obj: object) => {
+
+        var objAsString = new JsonPipe().transform(obj);
+        var senderTime = new Date();
+        var messageToSend = senderTime.toLocaleString() + ":  " + this.message;
+  
+        const text = `(${senderTime.toLocaleString()}) [received]: ${objAsString}`;
+        console.log(`arrived from [received]: ${objAsString}`);
+        this.messages.unshift(text);
+      });
 
       this._hubConnection
       .start()
@@ -84,7 +96,20 @@ export class ChatMessagesComponent implements OnInit {
       var messageToSend = this.message;
       this.message = '';
       this._hubConnection
-        .send('sendToAll', this.nick, messageToSend)
+        // .send('sendToAll', this.nick, messageToSend)
+        .send('Send', messageToSend)
+        .catch(err => console.error(err));
+      }
+    
+    public startMoving(direction: string): void {
+      this._hubConnection
+        .send('StartMoving', direction)
+        .catch(err => console.error(err));
+      }
+      
+    public stopMoving(): void {
+      this._hubConnection
+        .send('StopMoving')
         .catch(err => console.error(err));
       }
 }
